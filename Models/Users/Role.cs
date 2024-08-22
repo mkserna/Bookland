9f144c338fcedf2f7c3c1119b123b5c1f52f7936
Z4pata <zapata.devs@gmail.com>
2024-08-22 07:03:38 -0500

    Feat: Se creo la clase Role.cs

---
+++ Models/Users/Role.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookland.Models.Users
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Rol { get; set; }
+
+        
+    }
+}
