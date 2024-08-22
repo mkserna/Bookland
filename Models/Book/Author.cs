9764b4195a45b3836bfda8d83b8e2db1849ba259
oscarcalle <oscarcalle0@gmail.com>
2024-08-22 07:12:20 -0500

    Feat: Se crea la clase Author

---
+++ Models/Book/Author.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookland.Models.Book
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
