c495c769722c0e258f1d604933f5db9f8237741e
Mosquera31 <leison3131@hotmail.com>
2024-08-22 10:54:41 -0500

    work in progress

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,32 +1,78 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bookland.Models;
+using Bookland.Database;
+using Microsoft.EntityFrameworkCore;
+using Bookland.Models.Books;
 
-public class BookController: Controller
+public class BookController : Controller
 {
-    private readonly ILogger<BookController>_logger;
+    private readonly ILogger<BookController> _logger;
 
-    private readonly BookDbContext  Context;
+    private readonly AppDbContext _context;
 
-    public BookController(ILogger<BookController>logger, BookDbContext context)
+    public BookController(ILogger<BookController> logger, AppDbContext context)
     {
         _logger = logger;
         _context = context;
     }
 
-     public IActionResult Index()
+    public IActionResult Index()
     {
         return View();
     }
 
-    //Todos  los libros View
+    //calling the view of the books 
 
-    public IActionResult books()// revisar que considad con la vista de libros 
+    public async Task<IActionResult> books()
     {
-         var allBooks = _context.vistadelibros.Tolist();
-         return View(allBooks);
+        var allBooks = await _context.Books.ToListAsync();
+        return View(allBooks);
     }
 
-    //para Eliminar 
+
+    public IActionResult Delete(int id)
+    {
+        var BookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+        _context.Books.Remove(BookInDb);
+        _context.SaveChanges();
+        return RedirectToAction("aqui va el view ");  // deleting  the view, waitig to get Mariana name of the view
+
+    }
+
+    // here I am finding it Updating that Book
+    public IActionResult UpdateBook(int id)
+    {
+        if (id != null)
+        {
+
+            var BookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+            return View(BookInDb);
+        }
+        return View();
+    }
+
+    // here showing The form of the book
+    public IActionResult CreateUpdateForm(Book model)
+    {
+        if (model.Id==0)
+        {
+            
+        }
+        else
+        {
+
+        }
+        try
+        {
+
+        }
+        catch
+        {
+
+        }
+        return View();
+ }
+
 
 }
